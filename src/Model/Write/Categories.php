43ae9153ceb9d800720f8eccbf66cea74122f4d4
Freek Gruntjes <fgruntjes@emico.nl>
2017-02-08 17:40:55 +0100

    Begin gemaakt met store export

--- src/Model/Write/Categories.php
+++ src/Model/Write/Categories.php
@@ -16,6 +16,11 @@ class Categories implements WriterInterface
     protected $iterator;
 
     /**
+     * @var int
+     */
+    protected $exportedCategories = [];
+
+    /**
      * Categories constructor.
      *
      * @param EavIterator $iterator
@@ -30,12 +35,60 @@ class Categories implements WriterInterface
      */
     public function write(Writer $writer, XmlWriter $xml)
     {
+        $storeId = 1;
+        $this->exportedCategories = [];
         $xml->startAttribute('categories');
+
         foreach ($this->iterator as $data) {
-            var_dump($data);
+            // Always write root category
+            if ($data['entity_id'] == 1) {
+                $this->writeCategory($xml, $storeId, $data);
+            } elseif ($data['is_active']) {
+                $this->writeCategory($xml, $storeId, $data);
+            }
+            $writer->flush();
         }
+
         $xml->endAttribute();
         $writer->flush();
+        return $this;
+    }
+
+    /**
+     * @param int $storeId
+     * @param int $entityId
+     * @return string
+     */
+    protected function getTweakwiseId($storeId, $entityId)
+    {
+        return $storeId . '-' . $entityId;
+    }
+
+    /**
+     * @param XmlWriter $xml
+     * @param int $storeId
+     * @param array $data
+     * @return $this
+     */
+    protected function writeCategory(XmlWriter $xml, $storeId, array $data)
+    {
+        $tweakwiseId = $this->getTweakwiseId($storeId, $data['entity_id']);
+
+        $this->exportedCategories[$tweakwiseId] = true;
+
+        $xml->startElement('category');
+        $xml->writeElement('categoryid', $data['entity_id']);
+        $xml->writeElement('rank', $data['position']);
+        $xml->writeElement('name', $data['name']);
+
+        if (isset($data['parent_id']) && $data['parent_id']) {
+            $xml->startElement('parents');
+            $xml->writeElement('categoryid', $this->getTweakwiseId($storeId, $data['parent_id']));
+            $xml->endElement(); // </parents>
+        }
+
+        $xml->endElement(); // </category>
+
         return $this;
     }
 }
