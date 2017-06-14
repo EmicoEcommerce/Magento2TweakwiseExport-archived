8ca70e5e2dad8c2d9c5e30faf5a3828425e566b8
Freek Gruntjes <fgruntjes@emico.nl>
2017-06-14 15:47:38 +0200

    Added failing unit test for empty (null value) attributes

--- tests/functional/Model/Write/Writer/EmptyAttributeCest.php
+++ tests/functional/Model/Write/Writer/EmptyAttributeCest.php
@@ -10,6 +10,8 @@ namespace Emico\TweakwiseExport\Model\Write\Writer;
 
 use Emico\TweakwiseExport\Model\Write\Writer;
 use FunctionalTester;
+use Magento\Catalog\Model\ProductRepository;
+use Magento\Catalog\Model\ResourceModel\Product;
 use Magento\Framework\App\Area;
 
 class EmptyAttributeCest
@@ -29,6 +31,26 @@ class EmptyAttributeCest
             ['Emico_TweakwiseExport::../tests/fixtures/product/empty-special-price.csv'],
             [self::PRODUCT_SKU]
         );
+
+
+        // Only with a raw insert like this we where able to insert an empty value in the special_price table for issue #6
+        /** @var ProductRepository $repository */
+        $repository = $i->getObject(ProductRepository::class);
+        /** @var Product $resource */
+        $resource = $i->getObject(Product::class);
+
+        $attribute = $resource->getAttribute('special_price');
+
+        $table = $attribute->getBackend()->getTable();
+        $product = $repository->get(self::PRODUCT_SKU);
+        $entityIdField = $attribute->getBackend()->getEntityIdField();
+
+        $data = [
+            $entityIdField => $product->getId(),
+            'attribute_id' => $attribute->getId(),
+            'value' => null,
+        ];
+        $resource->getConnection()->insertOnDuplicate($table, $data, ['value']);
     }
 
     /**
@@ -36,6 +58,24 @@ class EmptyAttributeCest
      */
     public function tryToTest(FunctionalTester $i)
     {
+        /** @var Writer $writer */
+        $writer = $i->getObject(Writer::class);
+        $resource = fopen('php://temp', 'w+');
+        $writer->write($resource);
+        rewind($resource);
 
+        $content = stream_get_contents($resource);
+        file_put_contents('/Volumes/WWW/tweakwise2-ce.dev/var/feeds/tweakwise.xml', $content);
+        $xml = simplexml_load_string($content);
+        foreach ($xml->xpath('//item/attributes/attribute') as $attributeElement) {
+            $name = (string) $attributeElement->name;
+            if ($name != 'special_price') {
+                continue;
+            }
+
+            $value = (string) $attributeElement->value;
+            $value = trim($value);
+            $i->assertNotEmpty($value);
+        }
     }
 }
