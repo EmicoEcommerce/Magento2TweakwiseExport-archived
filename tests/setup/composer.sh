6882e51cb9edfc0b544077a8c760db932a1f3623
Freek Gruntjes <fgruntjes@emico.nl>
2017-08-04 16:48:45 +0200

    global composer install

--- tests/setup/composer.sh
+++ tests/setup/composer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Configure composer auth
-if [ -n "$GITHUB_TOKEN" ]; then composer config github-oauth.github.com ${GITHUB_TOKEN}; fi;
-if [ -n "$MAGENTO_TOKEN_PRIVATE" ]; then composer config http-basic.repo.magento.com ${MAGENTO_TOKEN_PUBLIC} ${MAGENTO_TOKEN_PRIVATE}; fi;
+if [ -n "$GITHUB_TOKEN" ]; then composer config --global github-oauth.github.com ${GITHUB_TOKEN}; fi;
+if [ -n "$MAGENTO_TOKEN_PRIVATE" ]; then composer --global config http-basic.repo.magento.com ${MAGENTO_TOKEN_PUBLIC} ${MAGENTO_TOKEN_PRIVATE}; fi;
 
 composer install
