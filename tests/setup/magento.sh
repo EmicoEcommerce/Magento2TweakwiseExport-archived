f54c841d1c62c2fed513fed48b43e86508af9bc0
Freek Gruntjes <fgruntjes@emico.nl>
2017-08-04 16:37:42 +0200

    Installed magento trough composer instead of clone

--- tests/setup/magento.sh
+++ tests/setup/magento.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Install Magento with dependencies
-git clone --depth=10 -b $MAGE2_VERSION https://github.com/magento/magento2 /tmp/build
-cd /tmp/build
+cd /tmp
+composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition:${MAGE2_VERSION} build
+cd build
 composer require emico/tweakwise-export
-composer install
 cp -R /home/travis/build/EmicoEcommerce/Magento2TweakwiseExport/* /tmp/build/vendor/emico/tweakwise-export
 php bin/magento setup:install --admin-email "$MAGE2_ADMIN_EMAIL" --admin-firstname "$MAGE2_ADMIN_FIRST_NAME" --admin-lastname "$MAGE2_ADMIN_LAST_NAME" --admin-password "$MAGE2_ADMIN_PASSWORD" --admin-user "$MAGE2_ADMIN_USERNAME" --backend-frontname admin --base-url "$MAGE2_FAKE_URL" --db-host "$MAGE2_DB_HOST" --db-name "$MAGE2_DB_NAME" --db-user "$MAGE2_DB_USER" --db-password "$MAGE2_DB_PASSWORD" --session-save files --use-rewrites 1 --use-secure 0 -vvv
 php bin/magento sampledata:deploy
