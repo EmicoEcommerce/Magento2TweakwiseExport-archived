c9efc8e8cf7687db5d903bd1739802fec35a2436
Freek Gruntjes <fgruntjes@emico.nl>
2017-08-15 11:34:18 +0200

    Travis build fix

--- tests/setup/composer.sh
+++ tests/setup/composer.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 # Configure composer auth
-if [ -n "$GITHUB_TOKEN" ]; then composer config --global github-oauth.github.com ${GITHUB_TOKEN}; fi;
-if [ -n "$MAGENTO_TOKEN_PRIVATE" ]; then composer --global config http-basic.repo.magento.com ${MAGENTO_TOKEN_PUBLIC} ${MAGENTO_TOKEN_PRIVATE}; fi;
+if [ -n "$GITHUB_TOKEN" ]; then
+    echo "Configure composer github token";
+    composer config --global --auth github-oauth.github.com ${GITHUB_TOKEN};
+fi;
+if [ -n "$MAGENTO_TOKEN_PRIVATE" ]; then
+    echo "Configure composer magento token";
+    composer --global config --auth http-basic.repo.magento.com ${MAGENTO_TOKEN_PUBLIC} ${MAGENTO_TOKEN_PRIVATE};
+fi;
 
 composer install
