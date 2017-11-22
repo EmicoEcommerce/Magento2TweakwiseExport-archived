01827fca9feaa8c85f5eda0ba1704ce16fe56ebe
Freek Gruntjes <fgruntjes@emico.nl>
2017-11-22 18:49:41 +1100

    Re enabled full magento install

--- tests/setup/magento.sh
+++ tests/setup/magento.sh
@@ -11,10 +11,18 @@ cp -vf ${TRAVIS_BUILD_DIR}/tests/setup/install-config-mysql.php dev/tests/integr
 
 echo "Install magento"
 php bin/magento setup:config:set --no-interaction \
+    --admin-email "$MAGE2_ADMIN_EMAIL" \
+    --admin-firstname "$MAGE2_ADMIN_FIRST_NAME" \
+    --admin-lastname "$MAGE2_ADMIN_LAST_NAME" \
+    --admin-password "$MAGE2_ADMIN_PASSWORD" \
+    --admin-user "$MAGE2_ADMIN_USERNAME" \
     --backend-frontname admin \
+    --base-url "$MAGE2_FAKE_URL" \
     --db-host "$MAGE2_DB_HOST" \
     --db-name "$MAGE2_DB_NAME" \
     --db-user "$MAGE2_DB_USER" \
     --db-password "$MAGE2_DB_PASSWORD" \
-    --session-save=files \
+    --session-save files \
+    --use-rewrites 1 \
+    --use-secure 0 \
     -vvv
