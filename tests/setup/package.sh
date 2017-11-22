f84ed2d4da0c374fd94ccebed6eda5f30ce8335f
Freek Gruntjes <fgruntjes@emico.nl>
2017-11-22 19:35:30 +1100

    Added faker install for building test environment

--- tests/setup/package.sh
+++ tests/setup/package.sh
@@ -6,4 +6,9 @@ cd /tmp/build
 composer require --no-interaction --ignore-platform-reqs emico/tweakwise-export
 cp -R ${TRAVIS_BUILD_DIR}/* /tmp/build/vendor/emico/tweakwise-export
 php bin/magento setup:upgrade --no-interaction
+
+# Install package dev dependencies. Unfortunately I do not have a generic way to do this yet
+composer require fzaninotto/faker --no-interaction
+
+# Make sure auto loading works as expected
 composer dump-autoload
