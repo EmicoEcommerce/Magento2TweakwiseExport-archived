e637b70f0b203beaebf8fb0a3450e1f6fd49d1b5
Freek Gruntjes <fgruntjes@emico.nl>
2017-11-27 18:42:37 +1100

    Moved multistore tetsts to there own namespace

--- src/Test/Integration/Export/MultiStoreTest.php
+++ src/Test/Integration/Export/MultiStoreTest.php
@@ -6,24 +6,15 @@
  * @license   Proprietary and confidential, Unauthorized copying of this file, via any medium is strictly prohibited
  */
 
-namespace Emico\TweakwiseExport\Test\Integration\Export\Product;
+namespace Emico\TweakwiseExport\Test\Integration\Export;
 
 use Emico\TweakwiseExport\Model\Config;
 use Emico\TweakwiseExport\Model\Helper;
 use Emico\TweakwiseExport\Test\Integration\ExportTest;
 use Emico\TweakwiseExport\TestHelper\Data\StoreProvider;
-use Magento\Catalog\Model\Product\Attribute\Source\Status;
 use Magento\TestFramework\Helper\Bootstrap;
 
-/**
- * Class MultiStoreBasicTest
- *
- * @package Emico\TweakwiseExport\Test\Integration\Export\Product
- *
- * @magentoDataFixtureBeforeTransaction createMultiStoreFixture
- * @magentoDbIsolation enabled
- */
-class MultiStoreBasicTest extends ExportTest
+abstract class MultiStoreTest extends ExportTest
 {
     /**
      * Store codes for second store
@@ -35,7 +26,7 @@ class MultiStoreBasicTest extends ExportTest
     /**
      * @var Helper
      */
-    protected $helper;
+    private $helper;
 
     /**
      * @var StoreProvider
@@ -78,45 +69,5 @@ class MultiStoreBasicTest extends ExportTest
         $storeProvider->removeStoreView(self::STORE_STORE_CODE);
         $storeProvider->removeStoreGroup(self::STORE_GROUP_CODE);
         $storeProvider->removeWebsite(self::STORE_WEBSITE_CODE);
-    }
-
-    /**
-     * Test multiple stores enabled
-     */
-    public function testEnabled()
-    {
-        $product = $this->productData->create();
-
-        $feed = $this->exportFeed();
-        $feed->getProduct($product->getId());
-        $feed->getProduct($product->getId(), self::STORE_STORE_CODE);
-    }
-
-    /**
-     * Test if feed will not be exported for disabled store
-     */
-    public function testDisabledStore()
-    {
-        $this->setConfig(Config::PATH_ENABLED, false);
-
-        $product = $this->productData->create();
-
-        $feed = $this->exportFeed();
-        $feed->assertProductMissing($product->getId());
-        $feed->getProduct($product->getId(), self::STORE_STORE_CODE);
-    }
-
-    /**
-     * Test if product will be skipped if disabled in store
-     */
-    public function testDisabledProductForOneStore()
-    {
-        $product = $this->productData->create();
-
-        $this->productData->saveAttribute($product, 'status', Status::STATUS_DISABLED, self::STORE_STORE_CODE);
-
-        $feed = $this->exportFeed();
-        $feed->getProduct($product->getId());
-        $feed->assertProductMissing($product->getId(), self::STORE_STORE_CODE);
     }
 }
