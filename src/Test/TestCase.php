95e51ed9a3523d59a6b500e9bc33d2bfe80b3519
Freek Gruntjes <fgruntjes@emico.nl>
2017-11-28 18:25:21 +1100

    Test case reference for 2.1 fix

--- src/Test/TestCase.php
+++ src/Test/TestCase.php
@@ -12,7 +12,7 @@ if (class_exists('PHPUnit\Framework\TestCase')) {
     abstract class BaseTestCase extends \PHPUnit\Framework\TestCase
     { }
 } else {
-    abstract class BaseTestCase extends \PHPUnit\Framework\TestCase
+    abstract class BaseTestCase extends \PHPUnit_Framework_TestCase
     { }
 }
 
