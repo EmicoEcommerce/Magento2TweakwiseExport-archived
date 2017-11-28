bc9a6f78a70a584ddc6570ad3dd1ffa4b449d24f
Freek Gruntjes <fgruntjes@emico.nl>
2017-11-28 18:57:35 +1100

    Converted test helper entity hydrator to not need ZF hydrator

--- src/TestHelper/Data/EntityHydrator.php
+++ src/TestHelper/Data/EntityHydrator.php
@@ -6,38 +6,38 @@
 
 namespace Emico\TweakwiseExport\TestHelper\Data;
 
-use Zend\Hydrator\ClassMethods;
-use Zend\Hydrator\HydratorInterface;
+use ReflectionClass;
+use ReflectionException;
+use Zend\Filter\Exception\InvalidArgumentException;
+use Zend\Filter\Exception\RuntimeException;
+use Zend\Filter\FilterChain;
+use Zend\Filter\FilterInterface;
+use Zend\Filter\Word\UnderscoreToCamelCase;
 
-class EntityHydrator implements HydratorInterface
+class EntityHydrator
 {
     /**
-     * @var ClassMethods
+     * @var string[]
      */
-    private $classMethods;
+    private $methodCache;
 
     /**
-     * EntityHydrator constructor.
+     * @var ReflectionClass
      */
-    public function __construct()
-    {
-        $this->classMethods = new ClassMethods();
-    }
+    private $reflectionCache;
 
     /**
-     * {@inheritdoc}
+     * @var FilterInterface
      */
-    public function extract($object)
-    {
-        if (method_exists($object, 'getData')) {
-            return $object->getData();
-        }
-
-        return $this->classMethods->extract($object);
-    }
+    private $fieldToMethodFilter;
 
     /**
-     * {@inheritdoc}
+     * @param array $data
+     * @param object $object
+     * @return object
+     * @throws InvalidArgumentException
+     * @throws ReflectionException
+     * @throws RuntimeException
      */
     public function hydrate(array $data, $object)
     {
@@ -46,6 +46,61 @@ class EntityHydrator implements HydratorInterface
             return $object;
         }
 
-        return $this->classMethods->hydrate($data, $object);
+        $class = \get_class($object);
+        foreach ($data as $field => $value) {
+            $method = $this->getMethod($class, $field);
+            if ($method) {
+                $object->$method($value);
+            }
+        }
+        return $object;
+    }
+
+    /**
+     * @return FilterInterface
+     * @throws InvalidArgumentException
+     */
+    private function getFieldToMethodFilter(): FilterInterface
+    {
+        if ($this->fieldToMethodFilter === null) {
+            $filter = new FilterChain();
+            $filter->attach(new UnderscoreToCamelCase());
+
+            $this->fieldToMethodFilter = $filter;
+        }
+        return $this->fieldToMethodFilter;
+    }
+
+    /**
+     * @param string $class
+     * @return ReflectionClass
+     * @throws ReflectionException
+     */
+    private function getReflectionClass(string $class): ReflectionClass
+    {
+        if (!isset($this->reflectionCache[$class])) {
+            $this->reflectionCache[$class] = new ReflectionClass($class);
+        }
+        return $this->reflectionCache[$class];
+    }
+
+    /**
+     * @param string $class
+     * @param string $field
+     * @return string|false
+     * @throws ReflectionException
+     * @throws InvalidArgumentException
+     * @throws RuntimeException
+     */
+    private function getMethod(string $class, string $field)
+    {
+        $key = $class . $field;
+        if (!isset($this->methodCache[$key])) {
+            $method = 'get' . $this->getFieldToMethodFilter()->filter($field);
+            $reflection = $this->getReflectionClass($class);
+
+            $this->methodCache[$key] = $reflection->hasMethod($method) ? $method : false;
+        }
+        return $this->methodCache[$key];
     }
 }
