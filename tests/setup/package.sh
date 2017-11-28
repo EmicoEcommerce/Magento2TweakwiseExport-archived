7737c07e726835477a8f31b2856bb46a7b2dae42
Freek Gruntjes <fgruntjes@emico.nl>
2017-11-28 18:16:11 +1100

    Added magento enable module command for test

--- tests/setup/package.sh
+++ tests/setup/package.sh
@@ -5,6 +5,8 @@ set -e
 cd /tmp/build
 composer require --no-interaction --ignore-platform-reqs emico/tweakwise-export
 cp -R ${TRAVIS_BUILD_DIR}/* /tmp/build/vendor/emico/tweakwise-export
+
+php bin/magento module:enable Emico_TweakwiseExport
 php bin/magento setup:upgrade --no-interaction
 
 # Install package dev dependencies. Unfortunately I do not have a generic way to do this yet
