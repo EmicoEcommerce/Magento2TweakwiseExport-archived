7a062c565d8621b5363f09297632eeec2428bba6
Freek Gruntjes <fgruntjes@emico.nl>
2018-04-03 13:54:26 +0200

    Started with fixes for attribute provider to be able to delete attribute option

--- src/TestHelper/Data/Product/AttributeProvider.php
+++ src/TestHelper/Data/Product/AttributeProvider.php
@@ -10,11 +10,13 @@ namespace Emico\TweakwiseExport\TestHelper\Data\Product;
 
 use Magento\Catalog\Model\Product;
 use Magento\Catalog\Setup\CategorySetup;
-use Magento\Eav\Api\AttributeOptionManagementInterface;
 use Magento\Eav\Api\Data\AttributeInterface;
-use Magento\Eav\Api\Data\AttributeOptionInterfaceFactory;
-use Magento\Eav\Api\Data\AttributeOptionLabelInterfaceFactory;
 use Magento\Eav\Model\Config as EavConfig;
+use Magento\Eav\Model\Entity\Attribute\Option;
+use Magento\Eav\Model\ResourceModel\Entity\Attribute\Option\CollectionFactory;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Store\Model\StoreManagerInterface;
 
 class AttributeProvider
 {
@@ -24,47 +26,44 @@ class AttributeProvider
     private $eavConfig;
 
     /**
-     * @var AttributeOptionManagementInterface
-     */
-    private $attributeOptionManagement;
-
-    /**
-     * @var AttributeOptionLabelInterfaceFactory
-     */
-    private $optionLabelFactory;
-
-    /**
-     * @var AttributeOptionInterfaceFactory
-     */
-    private $optionFactory;
-
-    /**
      * @var CategorySetup
      */
     private $categorySetup;
 
     /**
+     * @var EavSetup
+     */
+    private $eavSetup;
+    /**
+     * @var CollectionFactory
+     */
+    private $attributeOptionCollectionFactory;
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
+
+    /**
      * AttributeProvider constructor.
      *
      * @param EavConfig $eavConfig
-     * @param AttributeOptionManagementInterface $attributeOptionManagement
-     * @param AttributeOptionLabelInterfaceFactory $optionLabelFactory
-     * @param AttributeOptionInterfaceFactory $optionFactory
      * @param CategorySetup $categorySetup
+     * @param EavSetup $eavSetup
+     * @param CollectionFactory $attributeOptionCollectionFactory
      */
     public function __construct(
         EavConfig $eavConfig,
-        AttributeOptionManagementInterface $attributeOptionManagement,
-        AttributeOptionLabelInterfaceFactory $optionLabelFactory,
-        AttributeOptionInterfaceFactory $optionFactory,
-        CategorySetup $categorySetup
+        CategorySetup $categorySetup,
+        EavSetup $eavSetup,
+        CollectionFactory $attributeOptionCollectionFactory,
+        StoreManagerInterface $storeManager
     )
     {
         $this->eavConfig = $eavConfig;
-        $this->attributeOptionManagement = $attributeOptionManagement;
-        $this->optionLabelFactory = $optionLabelFactory;
-        $this->optionFactory = $optionFactory;
         $this->categorySetup = $categorySetup;
+        $this->eavSetup = $eavSetup;
+        $this->attributeOptionCollectionFactory = $attributeOptionCollectionFactory;
+        $this->storeManager = $storeManager;
     }
 
     /**
@@ -72,6 +71,7 @@ class AttributeProvider
      *
      * @param string $code
      * @return AttributeInterface
+     * @throws LocalizedException
      */
     public function get(string $code): AttributeInterface
     {
@@ -81,6 +81,7 @@ class AttributeProvider
     /**
      * @param string $set
      * @return int
+     * @throws LocalizedException
      */
     public function getSetId(string $set = 'Default'): int
     {
@@ -104,32 +105,45 @@ class AttributeProvider
      * @param string $code
      * @param string $label
      * @return int
+     * @throws LocalizedException
      */
     public function getOptionId(string $code, string $label): int
     {
         $attribute = $this->get($code);
-        $options = $attribute->getOptions();
-        foreach ($options as $option) {
-            if (\mb_strtolower($option->getLabel()) === \mb_strtolower($label)) {
-                return (int) $option->getValue();
-            }
+        $collection = $this->attributeOptionCollectionFactory->create();
+        $collection->setAttributeFilter($attribute->getAttributeId());
+        $collection->addFilter('tdv.value', $label);
+        $collection->setStoreFilter($this->storeManager->getStore()->getId());
+
+        /** @var Option $option */
+        $option = $collection->getFirstItem();
+        if ($option->getId()) {
+            return $option->getId();
         }
 
-        // Create label
-        $optionLabel = $this->optionLabelFactory->create();
-        $optionLabel->setStoreId(0);
-        $optionLabel->setLabel($label);
-
-        // Create value
-        $option = $this->optionFactory->create();
-        $option->setLabel($optionLabel);
-        $option->setStoreLabels([$optionLabel]);
-        $option->setSortOrder(0);
-        $option->setIsDefault(false);
-
-        $this->attributeOptionManagement->add(Product::ENTITY, $attribute->getAttributeId(), $option);
+        $this->eavSetup->addAttributeOption([
+            'values' => [0 => $label],
+            'attribute_id' => $attribute->getAttributeId(),
+        ]);
 
         $attribute->setOptions();
         return $this->getOptionId($code, $label);
+    }
+
+    /**
+     * @param string $code
+     * @param string $label
+     * @throws LocalizedException
+     */
+    public function deleteOption(string $code, string $label)
+    {
+        $attribute = $this->get($code);
+        $optionId = $this->getOptionId($code, $label);
+        $this->eavSetup->addAttributeOption([
+            'value' => [$optionId => ''],
+            'delete' => [$optionId => true],
+            'attribute_id' => $attribute->getAttributeId(),
+        ]);
+        $attribute->setOptions();
     }
 }
