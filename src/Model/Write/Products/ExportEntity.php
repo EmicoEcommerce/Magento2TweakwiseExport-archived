d8e3aeb317722cb49597a8bc705c1a07c6c56bc6
Edwin Jacobs <ejacobs@emico.nl>
2019-08-07 12:26:55 +0200

    Changed shouldExportByName check

--- src/Model/Write/Products/ExportEntity.php
+++ src/Model/Write/Products/ExportEntity.php
@@ -542,6 +542,6 @@ class ExportEntity
      */
     protected function shouldExportByNameAttribute(): bool
     {
-        return !(empty($this->attributes['name']) || empty($this->getAttribute('name',false)));
+        return !empty($this->getName());
     }
 }
