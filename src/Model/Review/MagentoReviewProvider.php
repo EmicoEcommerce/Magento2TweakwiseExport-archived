c87123060a372e372ad20e0b291b36911c204866
Edwin Jacobs <ejacobs@emico.nl>
2019-12-04 16:07:18 +0100

    Created magento native review provider

--- src/Model/Review/MagentoReviewProvider.php
+++ src/Model/Review/MagentoReviewProvider.php
@@ -7,6 +7,8 @@
 namespace Emico\TweakwiseExport\Model\Review;
 
 use Emico\TweakwiseExport\Model\Write\Products\Collection;
+use Magento\Review\Model\ResourceModel\Review\Summary\CollectionFactory as SummaryCollectionFactory;
+use Magento\Review\Model\Review\Summary;
 
 /**
  * Class MagentoReviewProvider
@@ -15,11 +17,49 @@ use Emico\TweakwiseExport\Model\Write\Products\Collection;
 class MagentoReviewProvider implements ReviewProviderInterface
 {
     /**
+     * @var SummaryCollectionFactory
+     */
+    protected $summaryCollectionFactory;
+
+    /**
+     * MagentoReviewProvider constructor.
+     * @param SummaryCollectionFactory $summaryCollectionFactory
+     */
+    public function __construct(
+        SummaryCollectionFactory $summaryCollectionFactory
+    ) {
+        $this->summaryCollectionFactory = $summaryCollectionFactory;
+    }
+
+    /**
      * @param Collection $collection Tweakwise product collection
      * @return ProductReviewSummary[]
      */
     public function getProductReviews(Collection $collection): array
     {
-        return [];
+        $summaryCollection = $this->summaryCollectionFactory->create()
+            ->addStoreFilter($collection->getStoreId())
+            ->addEntityFilter($collection->getAllIds());
+
+        $reviews = [];
+        /** @var Summary $rating */
+        foreach ($summaryCollection as $summary) {
+            $reviews[] = $this->createProductReviewSummary($summary);
+        }
+
+        return $reviews;
+    }
+
+    /**
+     * @param Summary $summary
+     * @return ProductReviewSummary
+     */
+    protected function createProductReviewSummary(Summary $summary): ProductReviewSummary
+    {
+        return new ProductReviewSummary(
+            $summary->getRatingSummary(),
+            $summary->getReviewsCount(),
+            $summary->getEntityPkValue()
+        );
     }
 }
