a3655767f1f1969b4c759cc1732c46565bf56881
Stijn Bernards <stijn@bernards.nl>
2022-02-15 22:00:43 +0100

    fix: correct constructor for category iterator

--- Model/Write/Categories/Iterator.php
+++ Model/Write/Categories/Iterator.php
@@ -6,38 +6,38 @@
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-namespace Emico\TweakwiseExport\Model\Write\Categories;
+namespace Emico\TweakwiseExport\Model\Write\Products;
 
 use Emico\TweakwiseExport\Model\Helper;
 use Emico\TweakwiseExport\Model\Write\EavIterator;
+use Emico\TweakwiseExport\Model\Write\Products\CollectionDecorator\DecoratorInterface;
+use Generator;
+use Magento\Catalog\Model\Product;
 use Magento\Eav\Model\Config as EavConfig;
-use Magento\Eav\Model\Entity\Attribute\AbstractAttribute;
-use Magento\Framework\DB\Select;
+use Magento\Framework\Event\Manager;
 use Magento\Framework\Model\ResourceModel\Db\Context as DbContext;
 use Emico\TweakwiseExport\Model\Config as EmicoConfig;
 
 /**
  * Class Iterator
- * @package Emico\TweakwiseExport\Model\Write\Categories
+ * @package Emico\TweakwiseExport\Model\Write\Products
  */
 class Iterator extends EavIterator
 {
     /**
-     * @var array
+     * @var ExportEntityFactory
      */
-    protected $entityBatchOrder = [
-        'level',
-        'path'
-    ];
+    protected $entityFactory;
 
     /**
-     * {@inheritDoc}
+     * @var CollectionFactory
      */
-    protected $eavSelectOrder = [
-        'path',
-        'entity_id',
-        'store_id'
-    ];
+    protected $collectionFactory;
+
+    /**
+     * @var DecoratorInterface[]
+     */
+    protected $collectionDecorators;
 
     /**
      * Iterator constructor.
@@ -46,63 +46,95 @@ class Iterator extends EavIterator
      * @param Helper $helper
      * @param EavConfig $eavConfig
      * @param DbContext $dbContext
-     * @param array|string[] $attributes
+     * @param ExportEntityFactory $entityFactory
+     * @param CollectionFactory $collectionFactory
+     * @param IteratorInitializer $iteratorInitializer
+     * @param DecoratorInterface[] $collectionDecorators
      * @param EmicoConfig $config
      */
     public function __construct(
         Helper $helper,
         EavConfig $eavConfig,
         DbContext $dbContext,
-        $attributes,
+        Manager $eventManager,
+        ExportEntityFactory $entityFactory,
+        CollectionFactory $collectionFactory,
+        IteratorInitializer $iteratorInitializer,
+        array $collectionDecorators,
         EmicoConfig $config
     ) {
         parent::__construct(
             $helper,
             $eavConfig,
             $dbContext,
-            'catalog_category',
-            $attributes,
-            $config->getBatchSizeCategories()
+            $eventManager,
+            Product::ENTITY,
+            [],
+            $config->getBatchSizeProducts()
         );
+
+        $this->entityFactory = $entityFactory;
+        $this->collectionFactory = $collectionFactory;
+        $this->collectionDecorators = $collectionDecorators;
+
+        $iteratorInitializer->initializeAttributes($this);
     }
 
     /**
      * {@inheritdoc}
      */
-    protected function getStaticAttributeSelect(array $attributes): array
+    public function getIterator()
     {
-        $selects = parent::getStaticAttributeSelect($attributes);
-
-        foreach ($selects as $select) {
-            $select->columns('path');
-        }
-
-        return $selects;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function createEavAttributeGroupSelect(string $group, array $attributes): Select
-    {
-        $select = parent::createEavAttributeGroupSelect($group, $attributes);
-
-        if ($this->helper->isEnterprise()) {
-            $select->columns('main_table.path');
-        } else {
-            /** @var AbstractAttribute $staticAttribute */
-            $staticAttribute = reset($this->getAttributeGroups()['_static']);
-
-            /** @var AbstractAttribute $eavAttribute */
-            $eavAttribute = reset($attributes);
-
-            $select->join(
-                $staticAttribute->getBackendTable(),
-                $staticAttribute->getBackendTable() . '.entity_id = ' . $eavAttribute->getBackendTable() . '.entity_id',
-                ['path']
+        $batch = $this->collectionFactory->create(['store' => $this->store]);
+        foreach (parent::getIterator() as $entityData) {
+            $entity = $this->entityFactory->create(
+                [
+                    'store' => $this->store,
+                    'data' => $entityData
+                ]
             );
+            if (!$entity->shouldProcess()) {
+                continue;
+            }
+
+            $batch->add($entity);
+
+            if ($batch->count() === $this->batchSize) {
+                // After PHP7+ we can use yield from
+                foreach ($this->processBatch($batch) as $processedEntity) {
+                    yield $processedEntity;
+                }
+                $batch = $this->collectionFactory->create(['store' => $this->store]);
+            }
         }
 
-        return $select;
+        // After PHP7+ we can use yield from
+        foreach ($this->processBatch($batch) as $processedEntity) {
+            yield $processedEntity;
+        }
+    }
+
+    /**
+     * @param Collection $collection
+     * @return Generator
+     */
+    protected function processBatch(Collection $collection)
+    {
+        if ($collection->count()) {
+            foreach ($this->collectionDecorators as $decorator) {
+                $decorator->decorate($collection);
+            }
+        }
+
+        foreach ($collection->getExported() as $entity) {
+            yield [
+                'entity_id' => $entity->getId(),
+                'name' => $entity->getName(),
+                'price' => $entity->getPrice(),
+                'stock' => (int) round($entity->getStockQty()),
+                'categories' => $entity->getCategories(),
+                'attributes' => $entity->getAttributes(),
+            ];
+        }
     }
 }
